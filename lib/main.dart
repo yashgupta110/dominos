8cc55e8e92b261406578de29a7d2be391997c4a1
Xiosmilus <ysgupta110@gmail.com>
2023-12-18 21:18:56 +0530

    env setup

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:dominos/screens/home.dart';
+import 'package:flutter/material.dart';
+
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.light,
+    seedColor: const Color.fromARGB(255, 52, 61, 186)
+  ),
+);
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home:  const Home(),
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+
+class Home extends StatefulWidget{
+  const Home({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: Center(
+          child: Text('Home'),
+        ),
+      );
+  }
+}
