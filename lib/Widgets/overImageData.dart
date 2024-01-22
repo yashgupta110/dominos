874d128404a1ef43b80e4efaa354b431c4fb9580
Xiosmilus <ysgupta110@gmail.com>
2024-01-22 23:43:29 +0530

    bestseller modefied

---
+++ lib/Widgets/overImageData.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:icons_flutter/icons_flutter.dart';
+
+class OverimageData extends StatelessWidget {
+  const OverimageData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('₹109',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white)),
+            Text(
+              'Regular | New Hand Tossed  >',
+              style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                  decoration: TextDecoration.underline,
+                  decorationStyle: TextDecorationStyle.solid),
+            ),
+          ],
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        SizedBox(
+          height: 50,
+          // width: 10,
+          child: FilledButton(
+            onPressed: () {
+              print('ontap');
+            },
+            style: ButtonStyle(
+              backgroundColor: const MaterialStatePropertyAll(
+                  Color.fromARGB(255, 186, 29, 17)),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                ),
+              ),
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  'Add',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                ),
+                Icon(Entypo.plus),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
